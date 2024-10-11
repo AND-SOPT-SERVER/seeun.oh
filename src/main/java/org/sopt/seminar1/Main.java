e0a1c68f97c126c38574903160556416d8a9beb3
ose0221 <ose0221@naver.com>
2024-10-11 18:16:52 +0900

    feat: 일기 복구 기능 구현

--- src/main/java/org/sopt/seminar1/Main.java
+++ src/main/java/org/sopt/seminar1/Main.java
@@ -85,6 +85,19 @@ public class Main {
                             final String inputBody = ConsoleIO.readLine();
                             server.patch(inputId, inputBody);
                         }
+                        case "RESTORE" -> {
+                            ConsoleIO.printLine("삭제된 일기 목록입니다.!");
+                            server.getDeletedList().forEach(deletedDiary -> {
+                                try {
+                                    ConsoleIO.printLine(deletedDiary.getId()+" : "+deletedDiary.getBody());
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            });
+                            ConsoleIO.printLine("복구할 일기의 id 를 입력하세요!");
+                            final String inputId = ConsoleIO.readLine();
+                            server.restore(inputId);
+                        }
                         case "FINISH" -> {
                             server.finish();
                         }
@@ -111,6 +124,7 @@ public class Main {
                     - POST : 일기 작성하기
                     - DELETE : 일기 제거하기
                     - PATCH : 일기 수정하기
+                    - RESTORE : 일기 복구하기
                     """;
         }
         private String getStartMessage() {
